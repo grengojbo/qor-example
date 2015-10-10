d03c9298e239a8150f40d8bc78ffd8667556a1a0
Oleg Dolya <oleg.dolya@gmail.com>
2015-10-10 11:32:51 +0300

    modify login page

--- main.go
+++ main.go
@@ -45,22 +45,24 @@ func main() {
 	})
 	r.GET("/login", func(c *gin.Context) {
 		c.HTML(200, "login.tmpl", gin.H{
-			// "roomid":    roomid,
+			"title": admin.Admin.SiteName,
 			// "nick":      nick,
 			"timestamp": time.Now().Unix(),
 		})
 	})
-	// r.POST("/login", func(c *gin.Context) {
+	r.POST("/login", func(c *gin.Context) {
 	// 	var form models.User
 	// 	// This will infer what binder to use depending on the content-type header.
-	// 	if c.Bind(&form) == nil {
-	// 		if form.User == "manu" && form.Password == "123" {
-	// 			c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
-	// 		} else {
-	// 			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-	// 		}
-	// 	}
-	// })
+		if c.Bind(&form) == nil {
+			if form.User == "admin" && form.Password == "admin" {
+				c.JSON(http.StatusOK, gin.H{"status": "success", "message": "Ok"})
+			} else {
+				c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized", "message": "User unauthorized"})
+			}
+		} else {
+				c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Bad request"})
+			}
+	})
 	r.GET("/logout", func(c *gin.Context) {
 		c.Redirect(http.StatusMovedPermanently, "/login")
 	})
