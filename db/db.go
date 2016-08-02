8fb70f826628f78593e1ca072a658e7d467e19e7
Oleg Dolya <oleg.dolya@gmail.com>
2016-08-02 14:38:41 +0300

    modify db

--- db/db.go
+++ db/db.go
@@ -26,7 +26,7 @@ func init() {
 
 	dbConfig := config.Config.DB
 	if config.Config.DB.Adapter == "mysql" {
-		DB, err = gorm.Open("mysql", fmt.Sprintf("%v:%v@/%v?parseTime=True&loc=Local", dbConfig.User, dbConfig.Password, dbConfig.Name))
+		DB, err = gorm.Open("mysql", fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=True&loc=Local&charset=utf8", config.Config.DB.User, config.Config.DB.Password, config.Config.DB.Host, config.Config.DB.Port, config.Config.DB.Name))
 	} else if config.Config.DB.Adapter == "postgres" {
 		DB, err = gorm.Open("postgres", fmt.Sprintf("user=%v password=%v dbname=%v sslmode=disable", dbConfig.User, dbConfig.Password, dbConfig.Name))
 	} else {
